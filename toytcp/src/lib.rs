7e031586d27446a76f9091d58d7b5d4281aa0a8a
rhoboro <rhoboro@gmail.com>
2022-12-30 11:50:18 +0900

    3.3

---
+++ toytcp/src/lib.rs
@@ -0,0 +1,19 @@
+mod packet;
+mod socket;
+pub mod tcp;
+mod tcpflags;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
